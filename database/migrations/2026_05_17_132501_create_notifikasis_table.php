eb654af5fb884871dda261e522dbe4a73e1a233c
unknown <belajartulus2021@gmail.com>
2026-05-18 06:21:14 +0700

    fix: delete duplicate notification migration

--- database/migrations/2026_05_17_132501_create_notifikasis_table.php
+++ database/migrations/2026_05_17_132501_create_notifikasis_table.php
@@ -5,12 +5,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    // public function up()
+    // {
+    //     Schema::create('notifikasis', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->unsignedBigInteger('user_id');
+    //         $table->unsignedBigInteger('disposisi_id')->nullable();
+    //         $table->text('message');
+    //         $table->boolean('is_read')->default(false);
+    //         $table->timestamps();
+            
+    //         $table->index(['user_id', 'disposisi_id']);
+    //     });
+    // }
     public function up()
-    {
+{
+    // Tambahkan baris IF ini untuk membungkus perintah Schema::create
+    if (!Schema::hasTable('notifikasis')) {
         Schema::create('notifikasis', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('disposisi_id')->nullable();
+            $table->foreignId('user_id');
+            $table->foreignId('disposisi_id')->nullable();
             $table->text('message');
             $table->boolean('is_read')->default(false);
             $table->timestamps();
@@ -18,6 +33,7 @@ return new class extends Migration
             $table->index(['user_id', 'disposisi_id']);
         });
     }
+}
 
     public function down()
     {
